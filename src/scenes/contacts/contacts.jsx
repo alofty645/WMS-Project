fafdc2ef39131855c32202e0399dc2d286804805
Aden Lofthouse <adenl@vypex.com>
2023-04-03 08:32:38 +1000

    removed mock data import

--- src/scenes/contacts/contacts.jsx
+++ src/scenes/contacts/contacts.jsx
@@ -1,7 +1,6 @@
 import { Box, useTheme } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataContacts } from "../../data/mockData";
 import Header from "../../components/Header";
 import Editbutton from "../../components/editbutton";
 import NewContact from "../forms/newcontact";
